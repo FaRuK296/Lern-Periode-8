8355229e915317424a3aa02961d53932a8fae4fe
FaRuK296 <f.hodzic.inf24@stud.bbbaden.ch>
2026-01-09 12:02:46 +0100

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,3 @@
+console.log("hello world");
+var username = "Faruk";
+console.log("Hello " + username, ",how are you today?");
---
+++ hello.ts
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+let username: string = "Faruk";
+console.log("Hello " + username, ",how are you today?");
+
+
+
