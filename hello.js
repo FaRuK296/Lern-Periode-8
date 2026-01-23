c3742f8607c51339f8d6ff6e10533cae8ec714cc
FaRuK296 <f.hodzic.inf24@stud.bbbaden.ch>
2026-01-23 11:57:39 +0100

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,33 +1,33 @@
 var options = ["rock", "paper", "scissors"];
 var randomNumber = Math.floor(Math.random() * 3);
-var selection = prompt("Enter rock, paper or scissors:");
+var selection = (prompt("Enter rock, paper or scissors:") || "").toLowerCase();
 var enemySelection = options[randomNumber];
-alert("Your choice: " + selection);
-alert("Your Oponents choice: " + enemySelection);
+console.log("Your choice: " + selection);
+console.log("Your Oponents choice: " + enemySelection);
 if (selection == enemySelection) {
-    alert("It's a tie!");
+    console.log("It's a tie!");
 }
 if (selection == "scissors") {
     if (enemySelection == "rock") {
-        alert("You've lost.");
+        console.log("You've lost.");
     }
     if (enemySelection == "paper") {
-        alert("You've won.");
+        console.log("You've won.");
     }
 }
 if (selection == "rock") {
     if (enemySelection == "paper") {
-        alert("You've lost.");
+        console.log("You've lost.");
     }
     if (enemySelection == "scissors") {
-        alert("You've won.");
+        console.log("You've won.");
     }
 }
 if (selection == "paper") {
     if (enemySelection == "scissors") {
-        alert("You've lost.");
+        console.log("You've lost.");
     }
     if (enemySelection == "rock") {
-        alert("You've won.");
+        console.log("You've won.");
     }
 }
--- hello.ts
+++ hello.ts
@@ -1,8 +1,12 @@
+type Option = "rock" | "paper" | "scissors";
+
 let options = ["rock", "paper", "scissors"];
 let randomNumber = Math.floor(Math.random() * 3);
 
-var selection = process.argv[2];
-var enemySelection = options[randomNumber]
+
+var selection = (prompt("Enter rock, paper or scissors:") || "").toLowerCase();
+
+var enemySelection = options[randomNumber];
 
 console.log("Your choice: " + selection);
 console.log("Your Oponents choice: " + enemySelection);
@@ -11,39 +15,32 @@ if (selection == enemySelection) {
     console.log("It's a tie!");
 }
 
-if(selection == "scissors")
-    
-    {
-        if (enemySelection == "rock") {
+if(selection == "scissors") {
+    if (enemySelection == "rock") {
         console.log("You've lost.");
-        }
-
-        if (enemySelection == "paper") {
+    }
+    if (enemySelection == "paper") {
         console.log("You've won.");
-        }
+    }
 }
 
-if(selection == "rock")
-    
-    {
-        if (enemySelection == "paper") {
+if(selection == "rock") {
+    if (enemySelection == "paper") {
         console.log("You've lost.");
-        }
-
-        if (enemySelection == "scissors") {
+    }
+    if (enemySelection == "scissors") {
         console.log("You've won.");
-        }
+    }
 }
 
-if(selection == "paper")
-    
-    {
-        if (enemySelection == "scissors") {
+if(selection == "paper") {
+    if (enemySelection == "scissors") {
         console.log("You've lost.");
-        }
-
-        if (enemySelection == "rock") {
+    }
+    if (enemySelection == "rock") {
         console.log("You've won.");
-        }
+    }
 }
+
+
 
